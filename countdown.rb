06da13ec72edb1d02c466cec76914e761be14187
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-09 19:12:54 -0700

    commit tree a582f9b7d24d0bbb3a5727f089f0ea170fd84324

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(x)
+    x = 10
+    while x > 0 
+      puts "#{x} SECOND(S)!"
+      x -= 1
+    end 
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(x)
+    x = 5
+     while x >= 0 
+        sleep(1)
+    x-=1
+    end
 end
